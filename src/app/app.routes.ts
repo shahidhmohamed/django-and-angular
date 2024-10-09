5f080dd8cd69312e11545ea3cfad8b5dffcdf5b0
shahidhmohamed <mohamedshahidh81@gmail.com>
2024-10-10 00:00:57 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './inventory/product/product.component';
+import { ProductScreenComponent } from './inventory/product-screen/product-screen.component';
+import { AppScreenComponent } from './app-screen/app-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppScreenComponent },
+    {path: 'inventory', component:ProductScreenComponent},
+];
+
+export default routes;
+
